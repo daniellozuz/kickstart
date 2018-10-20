04ffc9bf7282a835e9591acd638902a24c868109
Daniel Zuziak <daniel.zuziak@gmail.com>
2018-10-21 00:54:53 +0200

    Implemented saver and refactored

--- common_anagrams/solve.py
+++ common_anagrams/solve.py
@@ -2,22 +2,19 @@ from collections import Counter
 
 
 class Problem(object):
-    current_problem_number = 0
-    def __init__(self, length, A, B):
-        Problem.current_problem_number += 1
-        self.number = Problem.current_problem_number
+    def __init__(self, number, length, A, B):
+        self.number = number
         self.length = int(length)
         self.A = A
         self.B = B
         self.solution = 0
 
     def solve(self):
-        print(f'Solving problem {self.number}')
         A_substrings, B_substrings = self.get_substrings()
         for A_substring in A_substrings:
             if self.has_anagram(A_substring, B_substrings):
                 self.solution += 1
-        print(self.solution)
+        return self.solution
 
     def get_substrings(self):
         A_substrings, B_substrings = [], []
@@ -36,19 +33,35 @@ class Problem(object):
 
 
 class Loader(object):
+    problem_number = 0
     def __init__(self, input_file):
         with open(input_file) as f:
             self.lines = [line.strip() for line in f.readlines()]
 
     def get_problems(self):
-        for problem in (zip(self.lines[1::3], self.lines[2::3], self.lines[3::3])):
-            yield Problem(*problem)
+        for problem_data in (zip(self.lines[1::3], self.lines[2::3], self.lines[3::3])):
+            Loader.problem_number += 1
+            yield Problem(Loader.problem_number, *problem_data)
 
     def get_header(self):
         return self.lines[0]
 
 
+class Saver(object):
+    problem_number = 0
+    def __init__(self, output_file):
+        self.f = open(output_file, 'w')
+
+    def save(self, solution):
+        Saver.problem_number += 1
+        self.f.write(f'Case #{Saver.problem_number}: {solution}\n')
+
 if __name__ == '__main__':
-    loader = Loader('A-small-practice.in')
+    FILENAME = 'A-small-practice'
+    loader = Loader(f'{FILENAME}.in')
+    saver = Saver(f'{FILENAME}.out')
     for problem in loader.get_problems():
-        problem.solve()
+        print(f'Solving problem: {problem.number}')
+        solution = problem.solve()
+        print(f'Solution: {solution}')
+        saver.save(solution)
